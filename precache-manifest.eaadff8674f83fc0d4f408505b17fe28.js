a769611474be250abbeb87cd9303c6dce0009d40
Irt <wahyu.teri@yahoo.com>
2020-01-17 17:37:29 +0700

    Updates

---
+++ precache-manifest.eaadff8674f83fc0d4f408505b17fe28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925740c7fe1eb2c106d2e89f8d5a615d",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "6f7aed50e5119b1c0b14",
+    "url": "/CV/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "64c160373bd689d405d4",
+    "url": "/CV/static/js/2.4bb628e8.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/CV/static/js/2.4bb628e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f7aed50e5119b1c0b14",
+    "url": "/CV/static/js/main.7535045c.chunk.js"
+  },
+  {
+    "revision": "047c35e920a3da1ba893",
+    "url": "/CV/static/js/runtime-main.cd9282f6.js"
+  }
+]);
